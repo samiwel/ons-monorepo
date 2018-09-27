cd9f2030d15291cccbb21e5cc5af92e4bc78e5d9
Tom Andrews <git@tomandrews.co.uk>
2018-09-27 14:35:01 +0100

    Update script to use commit range for master but not branches

--- .travis/build-condition.sh
+++ .travis/build-condition.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 set -e
 
-
 if [[ -z $1 ]]; then
-    echo "Commit range cannot be empty"
-    exit 1
-fi
-
-if [[ -z $2 ]]; then
     echo "Change path cannot be empty"
     exit 1
 fi
 
-echo "$1"
-echo "$2"
+commits="master...$TRAVIS_BRANCH"
+if [[ $TRAVIS_BRANCH = "master" ]]; then
+    commits=$TRAVIS_COMMIT_RANGE
+fi
 
-git diff --name-only $1 | sort -u | uniq | grep $2 > /dev/null
+git diff --name-only $commits | sort -u | uniq | grep $1 > /dev/null
