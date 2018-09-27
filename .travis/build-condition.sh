824cfadc2ec883820b705fc5128e722935905bbe
Tom Andrews <git@tomandrews.co.uk>
2018-09-27 13:50:23 +0100

    Update script

--- .travis/build-condition.sh
+++ .travis/build-condition.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 
 if [[ -z $1 ]]; then
     echo "Commit range cannot be empty"
@@ -9,5 +11,8 @@ if [[ -z $2 ]]; then
     echo "Change path cannot be empty"
     exit 1
 fi
+
+echo "$1"
+echo "$2"
 
 git diff --name-only $1 | sort -u | uniq | grep $2 > /dev/null
